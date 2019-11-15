ed227c2b7fdf1344ba34c4f12848e79c3d8e1011
MrChen <examp@qq.com>
2019-11-15 17:05:35 +0800

    sql

---
+++ cjjblog.sql
@@ -0,0 +1,174 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : php7虚拟机
+ Source Server Type    : MySQL
+ Source Server Version : 50521
+ Source Host           : 192.168.5.183:3306
+ Source Schema         : cjjblog
+
+ Target Server Type    : MySQL
+ Target Server Version : 50521
+ File Encoding         : 65001
+
+ Date: 15/11/2019 17:04:21
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for admin
+-- ----------------------------
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE `admin`  (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `admin_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '管理员名',
+  `password` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '用户密码',
+  `mobile` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '手机号码',
+  `last_login_time` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '最后一次登录时间',
+  `status` smallint(4) UNSIGNED NOT NULL DEFAULT 0 COMMENT '状态0禁用1正常',
+  `create_time` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '账号创建时间',
+  `update_time` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '信息更新时间',
+  `is_delete` tinyint(4) UNSIGNED NOT NULL DEFAULT 0 COMMENT '是否删除',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_user`(`admin_name`, `mobile`, `status`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '管理员表' ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for admin_menu
+-- ----------------------------
+DROP TABLE IF EXISTS `admin_menu`;
+CREATE TABLE `admin_menu`  (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '菜单表',
+  `pid` int(11) UNSIGNED NULL DEFAULT 0 COMMENT '所属菜单',
+  `name` varchar(15) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '' COMMENT '菜单名称',
+  `mca` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '' COMMENT '模块、控制器、方法',
+  `ico` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '' COMMENT 'font-awesome图标',
+  `order_number` int(11) UNSIGNED NULL DEFAULT NULL COMMENT '排序',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 38 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '菜单权限表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of admin_menu
+-- ----------------------------
+INSERT INTO `admin_menu` VALUES (1, 0, '系统设置', 'Admin/ShowNav/config', 'cog', 1);
+INSERT INTO `admin_menu` VALUES (2, 1, '菜单管理', 'Admin/Nav/index', NULL, NULL);
+INSERT INTO `admin_menu` VALUES (7, 4, '权限管理', 'Admin/Rule/index', '', 1);
+INSERT INTO `admin_menu` VALUES (4, 0, '权限控制', 'Admin/ShowNav/rule', 'expeditedssl', 2);
+INSERT INTO `admin_menu` VALUES (8, 4, '用户组管理', 'Admin/Rule/group', '', 2);
+INSERT INTO `admin_menu` VALUES (9, 4, '管理员列表', 'Admin/Rule/admin_user_list', '', 3);
+INSERT INTO `admin_menu` VALUES (16, 0, '会员管理', 'Admin/ShowNav/', 'users', 4);
+INSERT INTO `admin_menu` VALUES (17, 16, '会员列表', 'Admin/User/index', '', NULL);
+INSERT INTO `admin_menu` VALUES (36, 0, '文章管理', 'Admin/ShowNav/posts', 'th', 0);
+INSERT INTO `admin_menu` VALUES (37, 36, '文章列表', 'Admin/Posts/index', '', NULL);
+
+-- ----------------------------
+-- Table structure for admin_role
+-- ----------------------------
+DROP TABLE IF EXISTS `admin_role`;
+CREATE TABLE `admin_role`  (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `admin_id` int(11) UNSIGNED NOT NULL DEFAULT 0 COMMENT '用户id',
+  `role_id` int(11) UNSIGNED NOT NULL DEFAULT 0 COMMENT '角色id',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '用户角色对应关系' ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for migrations
+-- ----------------------------
+DROP TABLE IF EXISTS `migrations`;
+CREATE TABLE `migrations`  (
+  `version` bigint(20) NOT NULL,
+  `migration_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `start_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `end_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `breakpoint` tinyint(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`version`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for permission
+-- ----------------------------
+DROP TABLE IF EXISTS `permission`;
+CREATE TABLE `permission`  (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '权限节点名称',
+  `type` smallint(4) UNSIGNED NOT NULL DEFAULT 0 COMMENT '权限类型1api权限2前路由权限',
+  `category_id` int(11) UNSIGNED NOT NULL DEFAULT 0 COMMENT '权限分组id',
+  `path` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '权限路径',
+  `path_id` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '路径唯一编码',
+  `description` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '描述信息',
+  `status` smallint(4) UNSIGNED NOT NULL DEFAULT 0 COMMENT '状态0未启用1正常',
+  `create_time` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '创建时间',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_permission`(`path_id`, `status`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '权限节点' ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for permission_category
+-- ----------------------------
+DROP TABLE IF EXISTS `permission_category`;
+CREATE TABLE `permission_category`  (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '权限分组名称',
+  `description` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '权限分组描述',
+  `status` smallint(4) UNSIGNED NOT NULL DEFAULT 1 COMMENT '权限分组状态1有效2无效',
+  `create_time` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '权限分组创建时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for role
+-- ----------------------------
+DROP TABLE IF EXISTS `role`;
+CREATE TABLE `role`  (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '角色名',
+  `description` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '角色描述',
+  `status` smallint(4) UNSIGNED NOT NULL DEFAULT 0 COMMENT '状态1正常0未启用',
+  `sort_num` int(11) UNSIGNED NOT NULL DEFAULT 0 COMMENT '排序值',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_role`(`status`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '角色' ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for role_permission
+-- ----------------------------
+DROP TABLE IF EXISTS `role_permission`;
+CREATE TABLE `role_permission`  (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `role_id` int(11) UNSIGNED NOT NULL DEFAULT 0 COMMENT '角色编号',
+  `permission_id` int(11) UNSIGNED NOT NULL DEFAULT 0 COMMENT '权限编号',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '角色权限对应表' ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '用户名',
+  `password` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '用户密码',
+  `mobile` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '手机号码',
+  `last_login_time` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '最后一次登录时间',
+  `status` smallint(4) UNSIGNED NOT NULL DEFAULT 0 COMMENT '状态0禁用1正常',
+  `create_time` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '账号创建时间',
+  `update_time` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '信息更新时间',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_user`(`user_name`, `mobile`, `status`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '用户表' ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for user_role
+-- ----------------------------
+DROP TABLE IF EXISTS `user_role`;
+CREATE TABLE `user_role`  (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) UNSIGNED NOT NULL DEFAULT 0 COMMENT '用户id',
+  `role_id` int(11) UNSIGNED NOT NULL DEFAULT 0 COMMENT '角色id',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '用户角色对应关系' ROW_FORMAT = Compact;
+
+SET FOREIGN_KEY_CHECKS = 1;
